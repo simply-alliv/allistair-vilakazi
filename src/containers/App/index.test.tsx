dc5868dde163f67e1d61e50c0547d23f74634fd8
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-08-09 19:05:01 +0200

    fix: update tests

--- src/containers/App/index.test.tsx
+++ src/containers/App/index.test.tsx
@@ -3,12 +3,34 @@ import { render } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from ".";
 
-test("renders coming soon", () => {
-  const { getByText } = render(
-    <Router>
-      <App />
-    </Router>
-  );
-  const linkElement = getByText(/Coming Soon/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App route", () => {
+  it("renders about button", () => {
+    const { getByText } = render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const buttonElement = getByText(/ABOUT/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("renders portfolio button", () => {
+    const { getByText } = render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const buttonElement = getByText(/PORTFOLIO/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("renders download button", () => {
+    const { getByText } = render(
+      <Router>
+        <App />
+      </Router>
+    );
+    const buttonElement = getByText(/DOWNLOAD FULL RESUME \(PDF\)/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
