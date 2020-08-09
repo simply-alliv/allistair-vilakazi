1c76e42a15a85f60a057f372d4eb25c8ffcb39da
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-08-09 17:33:26 +0200

    fix: add route to test

--- src/containers/App/index.test.tsx
+++ src/containers/App/index.test.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from ".";
 
 test("renders coming soon", () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Router>
+      <App />
+    </Router>
+  );
   const linkElement = getByText(/Coming Soon/i);
   expect(linkElement).toBeInTheDocument();
 });
